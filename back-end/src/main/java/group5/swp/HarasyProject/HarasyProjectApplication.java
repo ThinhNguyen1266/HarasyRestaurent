a0fd2b366fed64fefe2d90b713343c64765acd4c
Nguyen Cuong Thinh <ThinhNgyenCTH@gmail.com>
2024-10-05 14:33:10 +0700

    init backend file

---
+++ back-end/src/main/java/group5/swp/HarasyProject/HarasyProjectApplication.java
@@ -0,0 +1,13 @@
+package group5.swp.HarasyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarasyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarasyProjectApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/group5/swp/HarasyProject/HarasyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.swp.HarasyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarasyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
