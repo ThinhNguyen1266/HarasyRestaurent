59c33ea6bfa6eb25b63e9c77512e9dd05e679806
DELL-PC\DELL <lieuphucthien2004@gmail.com>
2024-10-07 13:46:07 +0700

    View Staff , Search by role , sorted by role follow alphabet

--- back-end/src/main/java/group5/swp/HarasyProject/config/SecurityConfig.java
+++ back-end/src/main/java/group5/swp/HarasyProject/config/SecurityConfig.java
@@ -27,7 +27,7 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
 
     String[] PUBLIC_ENDPOINT = {
-            "/users","/regis/user","/auth/**","/branch","/uploadImage","/profile/{AccountId}"
+            "/users","/regis/user","/auth/**","/branch","/uploadImage","/profile/{AccountId}","/menus","/menu/{id}"
     };
 
 
@@ -39,6 +39,10 @@ public class SecurityConfig {
                 requests
                         .requestMatchers(PUBLIC_ENDPOINT).permitAll()
                         .requestMatchers(HttpMethod.GET,"/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/staff","/sorted","/staff/{role}").hasRole("BRANCH_MANAGER")
+                        .requestMatchers(HttpMethod.POST,"/menu").hasRole("BRANCH_MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/menu/{id}").hasRole("BRANCH_MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/menu/{id}").hasRole("BRANCH_MANAGER")
                         .anyRequest().authenticated()
         );
         httpSecurity.oauth2ResourceServer(oauth2->
